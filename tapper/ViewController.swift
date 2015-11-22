94ccb696b7c69b3a428e6db04d1cca75ee11b761
Chris Rathnam <Rathnam.C@gmail.com>
2015-11-22 00:24:32 -0500

    Tapper App iOS Development

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var coinBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver(){
+            restartGame()
+        }
+        
     }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        
+        
+        //handling input validation
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            coinBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            //This line will grap text and convert it into an Int, placing it in maxTaps
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            updateTapsLabel()
+        }
     }
+    
+    
+    //Function to restart game
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        coinBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    
+    //Function check if game is over
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    //Function to update Tap Label
+    func updateTapsLabel() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
 
+    
 
 }
 
